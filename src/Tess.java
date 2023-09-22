be933b30e8b9342fea34e36df8353b84271c7374
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2023-09-22 23:46:07 +0700

    get eselon baris biasa clear

--- src/Tess.java
+++ src/Tess.java
@@ -3,8 +3,14 @@ import utilities.Matriks;
 public class Tess {
     public static void main(String[] args) {
         System.out.println("asd");
-        Matriks m = new Matriks(3);
-        m.read();
+        Matriks m = new Matriks(6);
 
+        m.displayAugmentedMatrix(6);
+        m.readSPLFromCLI(6);
+        m.displayAugmentedMatrix(6);
+        m.getRowEchelon(6);
+        System.out.println("asd");
+
+        m.displayAugmentedMatrix(6);
     }
 }
--- src/utilities/Matriks.java
+++ src/utilities/Matriks.java
@@ -10,12 +10,30 @@ public class Matriks {
         // untuk augmented matrix
         Scanner objScan = new Scanner(System.in);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; i < n + 1; j++) {
+            for (int j = 0; j < n + 1; j++) {
                 int temp = objScan.nextInt();
                 mtrx[i][j] = temp;
             }
         }
         objScan.close();
+    }
+
+    public void displayAugmentedMatrix(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                System.out.print(this.mtrx[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void displayMatrix(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(this.mtrx[i][j] + " ");
+            }
+            System.out.println("\n");
+        }
     }
 
     public void readFromCLI(int n) {
@@ -59,9 +77,9 @@ public class Matriks {
     public void swapRowMatrix(int a, int b, int col) {
         for (int i = 0; i < col; i++) {
             double temp;
-            temp = this.mtrx[a][col];
-            this.mtrx[a][col] = this.mtrx[b][col];
-            this.mtrx[b][col] = temp;
+            temp = this.mtrx[a][i];
+            this.mtrx[a][i] = this.mtrx[b][i];
+            this.mtrx[b][i] = temp;
         }
     }
 
@@ -81,15 +99,75 @@ public class Matriks {
         return true;
     }
 
-    public void substractRow(int a, int b, double n, int col) {
+    public void substract2Row(int a, int b, double n, int col) {
         // row a - n*row b
         for (int i = 0; i < col; i++) {
             this.mtrx[a][i] -= (n * this.mtrx[b][i]);
         }
     }
 
-    public void getRowEchelon(int n) {
+    public void add2Row(int a, int b, double n, int col) {
+        // row a - n*row b
+        for (int i = 0; i < col; i++) {
+            this.mtrx[a][i] += (n * this.mtrx[b][i]);
+        }
+    }
 
+    public void divideRow(int a, double n, int col) {
+        // row a - n*row b
+        for (int i = 0; i < col; i++) {
+            this.mtrx[a][i] /= n;
+        }
+    }
+
+    public void getRowEchelon(int n) {
+        // cek yang kosongngan semua taruh ke bawah
+        for (int i = 0; i < n; i++) {
+            if (isRowZero(i, n)) {
+                for (int x = i + 1; x < n; x++) {
+                    swapRowMatrix(i, x, n + 1);
+                }
+            }
+        }
+        int col = 0;
+        for (int i = 0; i < n; i++) {
+            if (!isRowZero(i, n)) {
+
+                if (this.mtrx[i][col] != 0) {
+                    divideRow(i, this.mtrx[i][col], n + 1);
+                    for (int j = i + 1; j < n; j++) {
+                        if (this.mtrx[j][col] != 0) {
+                            substract2Row(j, i, this.mtrx[j][col] / this.mtrx[i][col], n + 1);
+                        }
+                    }
+                } else {
+                    if (isColZero(col, n)) {
+                        i--;
+                    } else {
+                        int temp = i;
+                        int idx = -1;
+                        while (temp < n && idx == -1) {
+                            if (this.mtrx[temp][col] != 0) {
+                                idx = temp;
+                            }
+                            temp++;
+                        }
+                        if (idx == -1) {
+                            i--;
+                        } else {
+                            add2Row(i, idx, 1, n + 1);
+                            divideRow(i, this.mtrx[i][col], n + 1);
+                            for (int j = i + 1; j < n; j++) {
+                                if (this.mtrx[j][col] != 0) {
+                                    substract2Row(j, i, this.mtrx[j][col] / this.mtrx[i][col], n + 1);
+                                }
+                            }
+                        }
+                    }
+                }
+                col++;
+            }
+        }
     }
 
     public Matriks(int n) {
