1003ffac64c3172c0f192caaf2dacc32c7c3b564
Edogawawa <nipponlegend72@gmail.com>
2023-09-23 13:38:42 +0700

    ini kali banteng commit

--- src/utilities/Matriks.java
+++ src/utilities/Matriks.java
@@ -316,6 +316,23 @@ public class Matriks {
         }
     }
 
+    
+    // return double matrix
+    // input e 2 matrix
+    public double[][] multiplyMatrix(double[][] a, double[][] b) {
+        double[][] c = new double[a.length][b[0].length];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                for (int k = 0; k < a[0].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+    
+    
+
     public double[][] getInverseMatriks() {
         return null;
     }
