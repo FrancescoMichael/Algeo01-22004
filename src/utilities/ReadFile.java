d5e56957eb88028a713f94b49b7861a805f69dfe
FrancescoMichael <francescokusuma15@gmail.com>
2023-09-26 20:25:38 +0700

    bikubik interpolasi

--- src/utilities/ReadFile.java
+++ src/utilities/ReadFile.java
@@ -149,4 +149,250 @@ public class ReadFile {
             e.printStackTrace();
         }
     }
+
+    public static void bicubicInterpolasi(){
+        try{
+            String[] tempData;
+            tempData = new String[1000];
+
+            Scanner myPath = new Scanner(System.in);
+            System.out.println("Input your file path : ");
+            String path = myPath.nextLine();
+            File myObj = new File(path);
+            Scanner myReader = new Scanner(myObj);
+
+            int rowMat = 0, colMat = 4;
+
+            // baca file
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                tempData[rowMat] = data;
+                rowMat++;
+            }
+
+            // convert baris terakhir tempData ke a dan b
+            double a, b;
+            double[] tempAB;
+            tempAB = new double[2];
+            int ind_tempAB = 0;
+            String tempIsi = "";
+            for(int i = 0; i < tempData[rowMat-1].length(); i++){
+                if(tempData[rowMat-1].charAt(i) == ' '){
+                    tempAB[ind_tempAB] = Double.valueOf(tempIsi);
+                    tempIsi = "";
+                    ind_tempAB +=1;
+                }else{
+                    tempIsi += tempData[rowMat-1].charAt(i);
+                }
+            }
+            a = tempAB[0];
+            b = Double.valueOf(tempIsi);
+            
+            // convert ke matrix
+            // mtrx berbentuk 4x4
+            double mtrx[][] = datatoMatrix(tempData, 4, 4); 
+
+            // menggunakan bentuk y = Xa, cari matrix a
+            // convert ke matrix y
+            double[][] y;
+            y = new double[16][1];
+            int ind_y = 0;
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    y[ind_y][0] = mtrx[i][j];
+                    ind_y++;
+                }
+            }
+
+            // convert ke matrix X;
+            double[][] X;
+            X = new double[16][16];
+            double xX = 0, yX = 0;
+
+            // f
+            int indRow = 0;
+            for( ; indRow < 4; indRow++){
+                if(indRow%4 == 0){
+                    xX = 0;
+                    yX = 0;
+                }else if(indRow%4 == 1){
+                    xX = 1;
+                    yX = 0;
+                }else if(indRow%4 == 2){
+                    xX = 0;
+                    yX = 1;
+                }else{
+                    xX = 1;
+                    yX = 1;
+                }
+                double suku1 = 0, suku2 = 0;
+
+                int indCol = 0;
+                for(int i = 0; i <= 3; i++){
+                    for(int j = 0; j <= 3; j++){
+                        if(xX == 0 && j == 0){
+                            suku1 = 1;
+                        }else{
+                            suku1 = Math.pow(xX, j);
+                        }
+
+                        if(yX == 0 && i == 0){
+                            suku2 = 1;
+                        }else{
+                            suku2 = Math.pow(yX, i);
+                        }
+
+                        X[indRow][indCol] = suku1*suku2;
+                        indCol++;
+                    }
+                }
+            }
+
+            // fx
+            for( ; indRow < 8; indRow++){
+                if(indRow%4 == 0){
+                    xX = 0;
+                    yX = 0;
+                }else if(indRow%4 == 1){
+                    xX = 1;
+                    yX = 0;
+                }else if(indRow%4 == 2){
+                    xX = 0;
+                    yX = 1;
+                }else{
+                    xX = 1;
+                    yX = 1;
+                }
+
+                double suku1 = 0, suku2 = 0;
+
+                int indCol = 0;
+                for(int j = 0; j < 4; j++){
+                    for(int i = 0; i < 4; i++){
+                        if(i == 0){
+                            X[indRow][indCol] = 0;
+                            indCol++;
+                        }else{
+                            if(xX == 0 && (i-1) <= 0){
+                                suku1 = 1;
+                            }else{
+                                suku1 = Math.pow(xX, i-1);
+                            }
+                            
+                            if(yX == 0 && j <= 0){
+                                suku2 = 1;
+                            }else{
+                                suku2 = Math.pow(yX, j);
+                            }
+                    
+                            X[indRow][indCol] = i*suku1*suku2;
+                            indCol++;
+                        }
+                    }
+                }
+            }
+
+            // fx
+            for( ; indRow < 12; indRow++){
+                if(indRow%4 == 0){
+                    xX = 0;
+                    yX = 0;
+                }else if(indRow%4 == 1){
+                    xX = 1;
+                    yX = 0;
+                }else if(indRow%4 == 2){
+                    xX = 0;
+                    yX = 1;
+                }else{
+                    xX = 1;
+                    yX = 1;
+                }
+
+                double suku1 = 0, suku2 = 0;
+
+                int indCol = 0;
+                for(int j = 0; j < 4; j++){
+                    for(int i = 0; i < 4; i++){
+                        if(j == 0){
+                            X[indRow][indCol] = 0;
+                            indCol++;
+                        }else{
+                            if(xX == 0 && i <= 0){
+                                suku1 = 1;
+                            }else{
+                                suku1 = Math.pow(xX, i);
+                            }
+                            
+                            if(yX == 0 && (j-1) <= 0){
+                                suku2 = 1;
+                            }else{
+                                suku2 = Math.pow(yX, j-1);
+                            }
+                    
+                            X[indRow][indCol] = j*suku1*suku2;
+                            indCol++;
+                        }
+                    }
+                }
+            }
+
+            // fxy
+            for( ; indRow < 16; indRow++){
+                if(indRow%4 == 0){
+                    xX = 0;
+                    yX = 0;
+                }else if(indRow%4 == 1){
+                    xX = 1;
+                    yX = 0;
+                }else if(indRow%4 == 2){
+                    xX = 0;
+                    yX = 1;
+                }else{
+                    xX = 1;
+                    yX = 1;
+                }
+
+                double suku1 = 0, suku2 = 0;
+
+                int indCol = 0;
+                for(int j = 0; j < 4; j++){
+                    for(int i = 0; i < 4; i++){
+                        if(xX == 0 && (i-1) <= 0){
+                            suku1 = 1;
+                        }else{
+                            suku1 = Math.pow(xX, i-1);
+                        }
+                            
+                        if(yX == 0 && (j-1) <= 0){
+                            suku2 = 1;
+                        }else{
+                            suku2 = Math.pow(yX, j-1);
+                        }
+                    
+                        X[indRow][indCol] = i*j*suku1*suku2;
+                        indCol++;
+                    }
+                }
+            }
+
+            for(int i = 0; i < 16; i++){
+                for(int j = 0; j < 16; j++){
+                    System.out.print(X[i][j]);
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+            // for(int i = 0; i < 16; i++){
+            //     System.out.println(y[i][0]);
+            // }
+
+
+            myPath.close();
+            myReader.close();
+        } catch(FileNotFoundException e){
+            System.out.println("Your file is wrong.");
+            e.printStackTrace();
+        }
+    }
 }
